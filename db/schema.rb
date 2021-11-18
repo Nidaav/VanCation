688bc55d133b4a0d030171428df78bea137f0c49
Yann Lochkareff <yann.lochkareff@gmail.com>
2021-11-18 13:10:11 +0100

    Updated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_110711) do
+ActiveRecord::Schema.define(version: 2021_11_18_100306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,9 @@ ActiveRecord::Schema.define(version: 2021_11_16_110711) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
